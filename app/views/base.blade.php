5610ba5d823fb9cf9eb22d803ee66d57c20bc853
ksdev <ks.cloudbox@gmail.com>
2014-12-13 17:15:31 +0100

    Small css changes

--- app/views/base.blade.php
+++ app/views/base.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Clear Organization of Your Gists">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
     <link
         rel="stylesheet"
